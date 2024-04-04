4ae2bda20b0f7f08e9e3ebfb6df47ffb9b18b8ad
ChocoWu <1327793532@qq.com>
2024-04-04 20:13:34 +0800

    update

--- Vitron/vitron/__init__.py
+++
@@ -1,2 +0,0 @@
-from .model import LlavaLlamaForCausalLM
-from .train import *
--- Vitron/vitron/model/region_extractor/builder.py
+++
@@ -1,6 +0,0 @@
-from .layer import RegionExtractor
-
-def build_region_extractor(config, delay_load=False, **kwargs):
-    # config.mm_hidden_size, config.hidden_size
-    return RegionExtractor(config.mm_hidden_size, config.hidden_size)
-    
