dac0046568c91afd5f137c85f83b311a055dad33
ChocoWu <1327793532@qq.com>
2024-04-23 22:19:30 +0800

    update

--- vitron/train/train.py
+++ vitron/train/train.py
@@ -217,25 +217,6 @@ def safe_save_model_for_hf_trainer(trainer: transformers.Trainer,
             else:
                 torch.save(weight_to_save, os.path.join(output_dir, f'mm_projector.bin'))
         return
-    # if getattr(trainer.args, "tune_region_mlp_adapter", False):
-    #     # Only save Adapter
-    #     keys_to_match = ['mm_projector']
-    #     if getattr(trainer.args, "use_im_start_end", False):
-    #         keys_to_match.extend(['embed_tokens', 'embed_in'])
-
-    #     weight_to_save = get_mm_adapter_state_maybe_zero_3(trainer.model.named_parameters(), keys_to_match)
-    #     trainer.model.config.save_pretrained(output_dir)
-
-    #     current_folder = output_dir.split('/')[-1]
-    #     parent_folder = os.path.dirname(output_dir)
-    #     if trainer.args.local_rank == 0 or trainer.args.local_rank == -1:
-    #         if current_folder.startswith('checkpoint-'):
-    #             mm_projector_folder = os.path.join(parent_folder, "mm_projector")
-    #             os.makedirs(mm_projector_folder, exist_ok=True)
-    #             torch.save(weight_to_save, os.path.join(mm_projector_folder, f'{current_folder}.bin'))
-    #         else:
-    #             torch.save(weight_to_save, os.path.join(output_dir, f'mm_projector.bin'))
-    #     return
 
     if trainer.deepspeed:
         print('trainer.deepspeed is true')
