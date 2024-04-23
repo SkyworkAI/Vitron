e99f957c5eae35b33c1ee35834fb90543380f6b0
ChocoWu <1327793532@qq.com>
2024-04-23 22:27:21 +0800

    update

--- vitron/train/train_mem.py
+++ vitron/train/train_mem.py
@@ -7,7 +7,7 @@ from llama_flash_attn_monkey_patch import replace_llama_attn_with_flash_attn
 
 replace_llama_attn_with_flash_attn()
 
-from train import train
+from vitron.train.train import train
 
 if __name__ == "__main__":
     train()
